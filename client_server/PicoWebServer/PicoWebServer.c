162e091ebd839f30e12f9fc7bf794c8efd588965
hex-plex <sendhilsom7@gmail.com>
2021-10-18 15:22:59 +0530

    finished communiication

--- client_server/PicoWebServer/PicoWebServer.c
+++ client_server/PicoWebServer/PicoWebServer.c
@@ -15,26 +15,52 @@
 //#define WIFISSID "jsparrow_2.4Ghz" // wifi SSID
 //#define WIFIPASS "happy@123" // wifi password
 
-#define WIFISSID "JioFiber-QycPA" // wifi SSID
-#define WIFIPASS "arunimap" // wifi password
-#define STATICIP "192.168.29.161" // static IP for PicoWebServer
-#define server_port 80
+#define WIFISSID "No" // wifi SSID
+#define WIFIPASS "abcdq1987654" // wifi password
+#define STATICIP "192.168.43.18" // static IP for PicoWebServer
+#define server_port 9999
 
-void fetch_Resp(char* buf, int timeout){
-  // char buf[512] = "";
-  int64_t prev_time = time_us_64(); 
-  while((time_us_64() - prev_time)<timeout*50){
-    if(uart_is_readable(uart0)){
-      // char re = uart_getc(uart0);
-      // char restr[1];
-      // restr[0] = re;
-      strcat(buf,(char *)uart_getc(uart0));
-    }
+
+void uart_flush(int ind){
+  switch(ind){
+    case 0:
+      while(uart_is_readable(uart0)){
+        uart_getc(uart0);
+      }
+      break;
+    case 1:
+      while(uart_is_readable(uart1)){
+        uart_getc(uart1);
+      }
+      break;
   }
+}
+
+void fetch_Resp(char* buf, const uint32_t waitTime){
+  // char buf[512] = "";
+  printf("%d\n", waitTime);
+  int64_t prev_time = time_us_64(); 
+  //uint32_t i=0;
+  while((int64_t)(time_us_64() - prev_time) < (int64_t)(waitTime)){
+    printf("%ld, %d |", (long)(time_us_64()-prev_time), waitTime);
+   
+    if(uart_is_readable(uart0)){
+      char re = uart_getc(uart0);
+      char restr[1];
+      restr[0] = re;
+      strcat(buf,restr); //(char *)uart_getc(uart0));
+     
+    }
+    sleep_us(30);
+    // if(i>10)break;
+
+    // i++;
+  }
+  strcat(buf,"\0");
   // return buf;
 }
 
-void sendCMD_waitResp(const char* cmd,int timeout){
+void sendCMD_waitResp(const char* cmd,const uint32_t waitTime){
   printf("CMD: %s",cmd);
     
   int sl = strlen(cmd);
@@ -43,15 +69,26 @@ void sendCMD_waitResp(const char* cmd,int timeout){
   uart_putc_raw(uart0, '\r');
   uart_putc_raw(uart0, '\n');
   // "\r\n");
-  //sleep_ms(timeout);
+  //sleep_ms(waitTime);
   char buf[512] = "";
-  fetch_Resp(buf, timeout);
+  fetch_Resp(buf, waitTime);
   printf("%s",buf);
 
   
 }
     
-
+void sendMsg(const char* msg){
+  char cmd[] = "AT+CIPSEND=";
+  int sl = strlen(msg);
+  char msglen[5];
+  itoa(sl, msglen, 10);
+  strcat(cmd, msglen);
+  for(int i=0;i<sl;i++)
+    uart_putc_raw(uart0, cmd[i]);
+  uart_putc_raw(uart0, '\r');
+  uart_putc_raw(uart0, '\n');
+  uart_puts(uart0, msg);
+}
 
 int main() {
   // stdio_init_all();
@@ -59,51 +96,35 @@ int main() {
   uart_init(uart0, 115200);
   gpio_set_function(0, GPIO_FUNC_UART);
   gpio_set_function(1, GPIO_FUNC_UART);
-  sleep_ms(1000);    // Give time for the USB to be setup
+  sleep_ms(5000);    // Give time for the USB to be setup
   // flash(3);       // Time to connect that USB port
-  sleep_ms(2000);
+  sleep_ms(5000);
+  uart_flush(0);
+
+
+  sendCMD_waitResp("AT",20000);
+  sleep_ms(1000);
+  sendCMD_waitResp("AT+CIFSR",20000);
+  sleep_ms(1000);
+  sendCMD_waitResp("AT+CIFSR",20000);
+  sleep_ms(1000);
+  sendCMD_waitResp("AT+CIPSTART=\"TCP\",\"192.168.43.18\",9999",20000);
+  sleep_ms(500);
+  sendMsg("");
 
   while(true){
-    sendCMD_waitResp("AT",2000);
-    printf("Hello\n");
-    sleep_ms(100);
+  //  printf("Hello\n");
+    // sendCMD_waitResp("AT",2000);
+    
+    // sleep_ms(1000);
+    char msg[10] = "";
+    printf("enter your message");
+    scanf("%s", msg);
+    sendCMD_waitResp("AT+CIPSTART=\"TCP\",\"192.168.43.18\",9999",20000);
+    sendMsg("");
+    sendMsg(msg);
   }
   
-
-  // sendCMD_waitResp("AT+CWMODE=3");
-
-
-  // sendCMD_waitResp("AT+CWLAP",10000);
-
-  // char j[] ="AT+CWJAP=";
-  // strcat(j, (char *)WIFISSID);
-  // strcat(j, ",");
-  // strcat(j, (char *)WIFIPASS);
-  // sendCMD_waitResp(j ,5000); //#Connect to AP
-  // sendCMD_waitResp("AT+CIFSR");    //#Obtain the Local IP Address
-  // sendCMD_waitResp("AT+CIPMUX=0");
-  // sendCMD_waitResp("AT+CIPSERVER=1,80");
-  // sleep_ms(10000);
-  // char k[] ="AT+CIPSTART=\"TCP\",\"";
-  // strcat(k, (char *)STATICIP);
-  // strcat(k, "\",");
-  // strcat(k, (char*)server_port);
-
-  // sendCMD_waitResp(k);
-  // while(true){
-  //   // printf("enter");
-  //   // string inputs; 
-  //   // scanf("%s",&inputs);
-  //   // string send[]="AT+CIPSEND";
-  //   // string size= itos(strlen(inputs));
-  //   // strcat(send, ;
-
-  //     printf(fetch_Resp());
-
-  //   // sendCMD_waitResp("AT+CIPSEND");
-
-  // }
-
   return 0;
 
 }
